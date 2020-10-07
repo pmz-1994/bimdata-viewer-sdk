f390f2f00ae25bd86a13888a46c8a33162b53da4
Amoki <hugo.duroux@gmail.com>
2020-10-07 14:03:03 +0200

    update Realiz3D plugin

--- src/plugins/realiz3D/src/Realiz3D.vue
+++ src/plugins/realiz3D/src/Realiz3D.vue
@@ -12,35 +12,23 @@ export default {
       iframeUrl: "",
     }
   },
-  props: {
-    active: Boolean // Listen to activation state
-  },
-  watch: {
-    active: {
-      handler(active) {
-        if (active) { // Is plugin activated
-          if (this.iframeUrl) {
-            this.$plugins.modalManager.pushModal({
-              template: `
-                <div style="margin:10px; height:95vh; width:95vw;">
-                  <iframe src="${this.iframeUrl}" height="100%" width="100%"></iframe>
-                </div>`
-            });
-          }
-          else {
-            this.$hub.emit('alert', {
-              type: 'error',
-              message: 'This plugin should be used on Socotec models only'
-            });
-          }
-        }
-      }
+  onOpen() {
+    if (this.iframeUrl) {
+      this.$viewer.globalContext.modals.pushModal({
+        template: `
+          <div style="margin:10px; height:95vh; width:95vw;">
+            <iframe src="${this.iframeUrl}" height="100%" width="100%"></iframe>
+          </div>`
+      })
+    } else {
+      this.$viewer.localContext.hub.emit("alert", {
+        type: "error",
+        message: 'This plugin should be used on Socotec models only',
+      });
     }
   },
-  methods: {
-  },
   created() {
-    const cloudId = parseInt(this.$utils.getCloudId());
+    const cloudId = parseInt(this.$viewer.api.cloudId);
     this.iframeUrl = cloudUrlMap[cloudId] || "";
   }
 };
--- src/plugins/realiz3D/src/realiz3D.plugin.js
+++ src/plugins/realiz3D/src/realiz3D.plugin.js
@@ -4,14 +4,15 @@ import icon from '../assets/logo.png';
 export default {
   name: 'Realiz3DPlugin', // The name of the plugin. this.$plugins.MyPluginName to access it from other plugins
   component: Realiz3DComponent,
-  display: {
-    iconPosition: 'right', // Add an icon in the left or right panel. Value can be "left" or "right"
-    content: 'free' // How the viewer show the plugin content. Value can be "windowed", "simple" of "free"
-  },
-  tooltip: 'tooltip', // Can be an i18n path or a string shown in all languages
-  keepActive: true, // If true, the window won't be closed if user click somewhere else.
-  icon: {
-    imgUri: icon,
+  addToWindows: ["3d"],
+  button: {
+    position: "right",
+    content: "free",
+    keepOpen: true,
+    tooltip: "Realiz3DPlugin.tooltip",
+    icon: {
+      imgUri: icon,
+    },
   },
   i18n: {
     en: {
